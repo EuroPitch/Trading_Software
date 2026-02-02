ccd1397f1fd965495d827c0fe419b7296e520286
Raj Aryan Upadhyaya <upadhyra@tcd.ie>
2026-02-02 16:33:06 +0000

    Fixed analytics further

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -2,7 +2,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
-import { Analytics } from "@vercel/analytics/next"
 
 export default function Layout() {
   return (
