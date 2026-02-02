5507b4ff8abd4d840204e2e14c36ecc074193f30
Raj Aryan Upadhyaya <upadhyra@tcd.ie>
2026-02-02 16:30:34 +0000

    Added vercel analytics

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -2,6 +2,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Layout() {
   return (
